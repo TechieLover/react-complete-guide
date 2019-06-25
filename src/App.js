5e31f26d783b22487142d05ccac3f561dac91599
Parwinder <parwinder.g@lynk.co.in>
2019-06-26 00:36:08 +0530

    new

--- src/App.js
+++ src/App.js
@@ -12,15 +12,19 @@ class App extends Component {
     showPersons:false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        {name:event.target.value,age:28},
-        {name:'Parwinder kumar',age:28},
-        {name:'goyal',age:28}
-    ]
+  nameChangedHandler = (event,id) => {
+    const personIndex = this.state.persons.findIndex(p =>{
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
 
-    })
+   // const person = Object.assign({},this.state.persons[personIndex]);
+   person.name = event.target.value;
+   const persons = [...this.state.persons]
+   persons[personIndex]=person;
+    this.setState({persons:persons });
   }
 
   deletePersonHandler = (personIndex)=>{
@@ -50,7 +54,8 @@ class App extends Component {
         <div >
           {this.state.persons.map((person,index) =>{
             return <Person click={()=>this.deletePersonHandler(index)} name={person.name} age={person.age}
-            key={person.id}/>
+            key={person.id}
+            changed={(event)=>this.nameChangedHandler(event,person.id)}/>
           })}
         </div>
       );
