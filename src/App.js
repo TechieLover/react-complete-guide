d758cc4fad2f1e0c0b368b00dd7dc1c1f42054d3
Parwinder <parwinder.g@lynk.co.in>
2019-06-25 23:15:01 +0530

    change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       {name:'Parwinder',age:28},
       {name:'goyal',age:28}
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons:false
   }
 
   switchNameHandler = (newName)=>{
@@ -33,6 +34,12 @@ class App extends Component {
 
     })
   }
+
+  togglePersonHandler = ()=>{
+      const doesShow = this.state.showPersons;
+      this.setState({showPersons:!doesShow})
+
+  }
   render() {
     const style ={
       backgroudColor: 'white',
@@ -45,12 +52,16 @@ class App extends Component {
       <div className="App">
        <h1>Hi , I'm a react App</h1>
        <p>This really works</p>
-       <button style={style} onClick={() => this.switchNameHandler("Random")}>Switch Name </button>
+       <button style={style} onClick={this.togglePersonHandler}>Switch Name </button>
+       {this.state.showPersons ? 
+       <div >
        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>I have some best things</Person>
        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
        click={this.switchNameHandler.bind(this,'Goyal family!!')}
        changed={this.nameChangedHandler}></Person>
        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+      </div> : null
+      }
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi Parwinder kumar Goyal'))
