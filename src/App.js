e978dbcb92cb0dd9a21952c483329629177f7d94
Parwinder <parwinder.g@lynk.co.in>
2019-06-26 00:18:13 +0530

    third

--- src/App.js
+++ src/App.js
@@ -12,18 +12,6 @@ class App extends Component {
     showPersons:false
   }
 
-  switchNameHandler = (newName)=>{
-    //console.log('was clicked');
-    //Don't due this  this.state.persons[0].name='Parwinde Kumar';
-    this.setState({ 
-      persons: [
-        {name:'Max',age:28},
-        {name:"Parwinder Goyal",age:29},
-        {name:'goyal',age:28}
-    ]
-  })
-  }
-
   nameChangedHandler = (event) => {
     this.setState({
       persons: [
@@ -33,6 +21,12 @@ class App extends Component {
     ]
 
     })
+  }
+
+  deletePersonHandler = (personIndex)=>{
+    const persons = this.state.persons;
+    persons.splice(personIndex,1);
+    this.setState({persons:persons});
   }
 
   togglePersonHandler = ()=>{
@@ -53,8 +47,8 @@ class App extends Component {
     if (this.state.showPersons){
       persons = (
         <div >
-          {this.state.persons.map(person =>{
-            return <Person name={person.name} age={person.age}/>
+          {this.state.persons.map((person,index) =>{
+            return <Person click={()=>this.deletePersonHandler(index)} name={person.name} age={person.age}/>
           })}
         </div>
       );
